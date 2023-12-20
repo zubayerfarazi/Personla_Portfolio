29a5127ebe5860b7dc20941994a9d3f9a1ac281a
zubayerforazi <forazizubayer@gmail.com>
2023-12-20 20:54:26 +0600

    Comitted

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // opening and closing Hambergar
-document.getElementById('openHam').addEventListener('click', function(){
-    document.getElementById('navigationItems').style.display = 'flex';
-    document.getElementById('closeHam').style.display = 'block';
-    document.getElementById('openHam').style.display = 'none';
-})
+document.getElementById("openHam").addEventListener("click", function () {
+  document.getElementById("navigationItems").style.display = "flex";
+  document.getElementById("closeHam").style.display = "block";
+  document.getElementById("openHam").style.display = "none";
+});
 
-document.getElementById('closeHam').addEventListener('click', function(){
-    document.getElementById('navigationItems').style.display = 'none';
-    document.getElementById('closeHam').style.display = 'none';
-    document.getElementById('openHam').style.display = 'block';
-})
+document.getElementById("closeHam").addEventListener("click", function () {
+  document.getElementById("navigationItems").style.display = "none";
+  document.getElementById("closeHam").style.display = "none";
+  document.getElementById("openHam").style.display = "block";
+});
 
 //Skills, Experiences, and Education
-document.getElementById('experience').addEventListener('click', function(){
-    document.getElementById('skills_li').style.display = 'none';
-    document.getElementById('experience_li').style.display = 'block';
-    document.getElementById('education_li').style.display = 'none';
-})
-document.getElementById('education').addEventListener('click', function(){
-    document.getElementById('skills_li').style.display = 'none';
-    document.getElementById('experience_li').style.display = 'none';
-    document.getElementById('education_li').style.display = 'block';
-})
-document.getElementById('skills').addEventListener('click', function(){
-    document.getElementById('skills_li').style.display = 'block';
-    document.getElementById('experience_li').style.display = 'none';
-    document.getElementById('education_li').style.display = 'none';
-})
+document.getElementById("experience").addEventListener("click", function () {
+  document.getElementById("skills_li").style.display = "none";
+  document.getElementById("experience_li").style.display = "block";
+  document.getElementById("education_li").style.display = "none";
+});
+document.getElementById("education").addEventListener("click", function () {
+  document.getElementById("skills_li").style.display = "none";
+  document.getElementById("experience_li").style.display = "none";
+  document.getElementById("education_li").style.display = "block";
+});
+document.getElementById("skills").addEventListener("click", function () {
+  document.getElementById("skills_li").style.display = "block";
+  document.getElementById("experience_li").style.display = "none";
+  document.getElementById("education_li").style.display = "none";
+});
 
 //sending mail
 // document.getElementById('goButton').addEventListener('click', function(){
@@ -34,54 +34,57 @@ document.getElementById('skills').addEventListener('click', function(){
 //     alert('Thanks For your Beautiful Words: ' + words);
 // })
 
-//Let us Talk Button 
-document.getElementById('letusTalk').addEventListener('click', function(){
-    let contactInfo = document.getElementById('card');
-    if (contactInfo.style.display === 'block') {
-        contactInfo.style.display = 'none';
-    } else {
-        contactInfo.style.display = 'block';
-    }
-})
+//Let us Talk Button
+document.getElementById("letusTalk").addEventListener("click", function () {
+  let contactInfo = document.getElementById("card");
+  if (contactInfo.style.display === "block") {
+    contactInfo.style.display = "none";
+  } else {
+    contactInfo.style.display = "block";
+  }
+});
 
 //API Fetch
 
-    setInterval(() => {
-        fetch('http://www.boredapi.com/api/activity/')
-        .then(res => res.json())
-        .then(data => {
-    document.getElementById('goButton').addEventListener('click', function(event){
-        document.getElementById('emailField').innerHTML = '';
-        
-        let createElement = document.createElement('p');
-        createElement.innerHTML = data.activity;
-       document.getElementById('emailField').appendChild(createElement);
-       document.getElementById('emailField').style.border = '2px solid red';
-       document.getElementById('emailField').style.borderRadius = '10px';
-       document.getElementById('emailField').style.padding = '10px';
-       document.getElementById('goButton').innerHTML = "Refresh Again"
-        
-        // console.log(data.activity);
-    })
+setInterval(() => {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((res) => res.json())
+    .then((data) => {
+      document
+        .getElementById("goButton")
+        .addEventListener("click", function (event) {
+          document.getElementById("emailField").innerHTML = "";
+
+          let createElement = document.createElement("p");
+          createElement.innerHTML = data.activity;
+          document.getElementById("emailField").appendChild(createElement);
+          document.getElementById("emailField").style.border = "2px solid red";
+          document.getElementById("emailField").style.borderRadius = "10px";
+          document.getElementById("emailField").style.padding = "10px";
+          document.getElementById("goButton").innerHTML = "Refresh Again";
+
+          // console.log(data.activity);
+        });
     }, 2000);
-})
+});
 
+const apiKey = "a43e5c8253adc3e603997f97943b4429";
+const cityName = "Dhaka";
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let weatherDegree = document.getElementById("degree");
+    let getWeather = document.createElement("p");
+    getWeather.innerText = (data.main.temp - 273.15).toFixed() + " °C";
 
-const apiKey = 'a43e5c8253adc3e603997f97943b4429';
-const cityName = 'Dhaka';
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    let weatherDegree = document.getElementById('degree');
-    let getWeather = document.createElement('p');
-    getWeather.innerText = (data.main.temp - 273.15).toFixed() + ' °C';
-
-    document.getElementById('modeOption').appendChild(getWeather);
-    document.getElementById('modeOption').style.fontSize = '20px';
-    document.getElementById('modeOption').style.border = '2px solid red';
-    document.getElementById('modeOption').style.borderRadius = '20px';
-    document.getElementById('modeOption').style.padding = '10px';
-    document.getElementById('modeOption').style.backgroundColor = 'white';
-    document.getElementById('modeOption').style.textAlign = 'center';
-    document.getElementById('modeOption').style.cursor = 'pointer';
-  })
+    document.getElementById("modeOption").appendChild(getWeather);
+    document.getElementById("modeOption").style.fontSize = "20px";
+    document.getElementById("modeOption").style.border = "2px solid red";
+    document.getElementById("modeOption").style.borderRadius = "20px";
+    document.getElementById("modeOption").style.padding = "10px";
+    document.getElementById("modeOption").style.backgroundColor = "white";
+    document.getElementById("modeOption").style.textAlign = "center";
+    document.getElementById("modeOption").style.cursor = "pointer";
+  });
